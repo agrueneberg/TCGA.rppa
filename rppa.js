d95275407a4c07264237bddf754fd41395856267
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2012-11-13 12:35:27 -0600

    Bootstrap AngularJS app.

---
+++ rppa.js
@@ -0,0 +1,26 @@
+(function () {
+    "use strict";
+
+ // Load dependencies.
+    TCGA.loadScript({
+        registerModules: false,
+        scripts: ["https://ajax.googleapis.com/ajax/libs/angularjs/1.0.2/angular.min.js"]
+    }, function () {
+
+        var app;
+
+        app = angular.module("app", []);
+
+     // Register tab.
+        TCGA.ui.registerTab({
+            id: "rppa",
+            title: "RPPA",
+            content: '<div class="page-header"><h1>RPPA <small>Real time analysis of reverse phase protein array data.</small></h1></div>',
+            switchTab: true
+        }, function (err, el) {
+            angular.bootstrap(el, ["app"]);
+        });
+
+    });
+
+}());
