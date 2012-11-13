b1a2513d821bfc02d3d09c6f404dee0456293786
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2012-11-13 12:37:13 -0600

    Add template controller.

--- rppa.js
+++ rppa.js
@@ -11,11 +11,19 @@
 
         app = angular.module("app", []);
 
+        app.controller("template", function ($scope, $templateCache) {
+            $templateCache.put("test.html", 'Hello World!');
+            $scope.template = "test.html";
+            $scope.$on("updateTemplate", function (event, template) {
+                $scope.template = template;
+            });
+        });
+
      // Register tab.
         TCGA.ui.registerTab({
             id: "rppa",
             title: "RPPA",
-            content: '<div class="page-header"><h1>RPPA <small>Real time analysis of reverse phase protein array data.</small></h1></div>',
+            content: '<div class="page-header"><h1>RPPA <small>Real time analysis of reverse phase protein array data.</small></h1></div><div ng-controller="template"><ng-include src="template" /></div>',
             switchTab: true
         }, function (err, el) {
             angular.bootstrap(el, ["app"]);
